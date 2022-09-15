bdd59cd0027101d1cf0dc827ca3979a8ba36dfda
pkr187 <pankajroman187@gmail.com>
2022-09-15 23:08:51 +0530

    [pankaj] Add:CreateLinkedList

---
+++ LinkedListProblem/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LinkedListProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            LinkedList list = new LinkedList();
+            while (flag)
+            {
+                Console.WriteLine("please Enter Your option :");
+                Console.WriteLine("1.Create a Linked List ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        list.Display();
+                        break;
+                }
+            }
+        }
+    }
+}
