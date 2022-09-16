42c4aa6ccd19153fd8cc20fd196b699e15a77b95
pkr187 <pankajroman187@gmail.com>
2022-09-16 23:21:25 +0530

    [pankaj] Add:DequeueElement

---
+++ LinkedListProblem/LinkedListQueue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    internal class LinkedListQueue
+    {
+        private Node top;
+        private Node head;
+        public LinkedListQueue()
+        {
+            this.top = null;
+        }
+        ///<summary>
+        ///UC-12
+        ///Create Queue
+        ///</summary>
+        ///<returns></returns>
+        internal void Enqueue(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to queue", value);
+        }
+        ///<summary>
+        ///UC-13
+        ///Dequeue Element
+        ///</summary>
+        ///<returns></returns>
+        internal void Dequeue()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Queue is Empty, Deletion is not Possible");
+                return;
+            }
+            else
+            {
+                while (this.head != null)
+                {
+                    Console.WriteLine("value Dequeue is {0}", this.head.data);
+                    this.head = this.head.next;
+                }
+            }
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
+    
+
---
+++ LinkedListProblem/LinkedListStack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    internal class LinkedListStack
+    {
+        private Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        ///<summary>
+        ///UC-10
+        ///Create Stack
+        ///</summary>
+        ///<returns></returns>
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        ///<summary>
+        ///UC-11
+        ///Peek & Pop from Stack
+        ///</summary>
+        ///<returns></returns>
+        internal void Peek() //to show the top most element
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack", this.top.data);
+        }
+        internal void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty, Deletion is not Possible");
+                return;
+            }
+            Console.WriteLine("Value Popped is {0}", this.top.data);
+            this.top = this.top.next;
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
+    
+
